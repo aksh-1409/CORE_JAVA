d3ae37d7756c5fa52070ac14d9fe3e4bef158a52
Aksh Usrathe <akshusrathe007@gmail.com>
2025-05-07 16:22:35 +0530

    string

---
+++ 27_string/A.java
@@ -0,0 +1,8 @@
+class A extends String {
+
+}
+
+/*A.java:1: error: cannot inherit from final String
+class A extends String {
+                ^
+1 error*/
---
+++ 27_string/B.java
@@ -0,0 +1,13 @@
+class B {
+    public static void main(String[] args) {
+        String a = "Ab *^$#@\n_98\u0041";
+
+        System.out.println(a);
+    }    
+}
+
+/*
+
+Ab *^$#@
+_98A
+*/
