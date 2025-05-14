12aca96d0528805851f13929b35c92c7aeecdd62
Aksh Usrathe <akshusrathe007@gmail.com>
2025-05-14 14:51:29 +0530

    type conversion

---
+++ 29_wrapper_classes/conversion_casting/B1.java
@@ -0,0 +1,11 @@
+class B1 {
+    public static void main(String[] args) {
+        byte x = (byte)234;
+        short y = (short)56000;
+        char z = (char)78000;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
