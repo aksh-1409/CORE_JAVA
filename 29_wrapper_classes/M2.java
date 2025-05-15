26f7ea78fd18d93f52bcff22d152d76517b68cb5
Aksh Usrathe <akshusrathe007@gmail.com>
2025-05-15 15:46:24 +0530

    wrapper classes

---
+++ 29_wrapper_classes/M2.java
@@ -0,0 +1,18 @@
+class M2 {
+
+   static Byte d;
+
+    public static void main(String[] args) {
+        Byte a = (byte)128;
+        Character b = (char)65536;
+        Short c = (short)32768;
+
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }    
+}
